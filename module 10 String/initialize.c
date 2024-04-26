9f04424794142c33cfddfe4ed5607acb9237bfee
Dark <istiak.csinfo@gmail.com>
2024-04-27 02:02:48 +0600

    twenty seven

--- module 10 String/initialize.c
+++ module 10 String/initialize.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int a[5]={10,20,30,40,50};
-    char b[6]={'i','s','t','i','a','k'};
-    char c[6]="Durlov"; //superpower of String
-    printf("%s",c)
+    // int a[5]={10,20,30,40,50};
+    // char b[6]={'i','s','t','i','a','k'}; // no guranty for take null"\0" in the end of the string
+    char c[7]="Durlov"; //superpower of String
+    // printf("%s\n",b);
+    printf("%s",c); //super power you can print without arrray
 
     return 0;
 }
+
+// char c[7]="Durlov"  automatically take an char "\0" after the end of string;
